7b26c155da1cb2c163c6b4c4e345eed5e61646c6
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-04 11:08:23 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,112 @@
+// var obj={
+//     name:"Aslam Khan",
+//     syptoms: "Cough flue fever",
+//     isCorona: false,
+//     isolatedSystem:false
+// }
+
+
+
+// var patient1={
+//     name:"Akram Khan",
+//     syptoms: "Cough fever",
+//     isCorona: true,
+//     isolatedSystem:true
+// }
+
+// var patient2={
+//     name:"Mushi Khan",
+//     syptoms: "Cough flue fever",
+//     isCorona: true,
+//     isolatedSystem:false
+
+
+// }
+// // var ptients=[obj,patient1, patient2]
+// for(var key in obj){
+
+//     document.write(obj[key])
+
+// }
+
+// console.log(obj)
+// console.log(patient1)
+// console.log(patient2)
+
+// console.log(obj)
+
+
+
+
+
+
+
+// var name=prompt("Enter Name");
+// var roll=prompt("Enter Roll Number");
+
+
+// var subject1=prompt("Enter Your First Favorite subject");
+// var subject2=prompt("Enter Your Second Favorite subject");
+
+
+// var student={
+//     name,
+//     roll,
+//     subjects:[subject1 , subject2]
+ 
+// }
+// for(var key in student){
+//     document.write([key]+":"+ student[key] + "<br>")
+// }
+
+// console.log(student)
+
+
+
+
+
+// var subject1=+prompt("Enter Your First Number subject");
+// var subject2=+prompt("Enter Your Second Number subject");
+
+
+// var sum=subject1+subject2;
+// console.log(sum)
+
+
+
+
+// var hobby1=prompt("Enter Your First Hobby subject");
+// var hobby2=prompt("Enter Your Second Hobby subject");
+// var hobby3=prompt("Enter Your Third Hobby subject");
+
+
+// var array=[hobby1,hobby2,hobby3]
+
+// console.log(array)
+
+
+
+
+
+
+
+
+
+    /* Function*/ 
+
+
+
+
+// function foodReady(e){
+//     console.log(e)
+// }
+// foodReady("Food is Ready!");
+
+
+function sum(){
+    var sum1=+prompt("Enter a Number")
+    var sum2=+prompt("Enter another Number")
+    console.log(sum1+sum2)
+}
+sum(    )
+
